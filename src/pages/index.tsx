00f99d15512dee66c700e243dcadc0fda24117c2
kingdaro <kingdaro@gmail.com>
2018-02-13 19:13:57 -0500

    update home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,12 +7,11 @@ const IndexPage = () => (
     <h1>hi there!</h1>
     <p>
       I'm Darius. I'm 21, currently live in Ohio, and I've been programming for
-      a little over eight years, in anything from games, mobile, web, or
-      whatever else I feel like dipping my toes into. Right now I'm focusing on
-      frontend web development, using React and TypeScript. You can view what
-      I'm working on at my <a href={social.github}>GitHub</a>. On occasion, if
-      there's an interesting subject that pops up every now and again, I'll blog
-      about it.
+      a few years, in anything from games, mobile, web, or whatever else I feel
+      like dipping my toes into. Right now I'm focusing on frontend web
+      development, using React and TypeScript. You can view what I'm working on
+      at my <a href={social.github}>GitHub</a>. On occasion, if there's an
+      interesting subject that pops up every now and again, I'll blog about it.
     </p>
     <p>
       I've been producing music for a while now as well, primarily electronic.
@@ -23,12 +22,12 @@ const IndexPage = () => (
       helping people out on Discord and Stack Overflow, being silly on{" "}
       <a href={social.twitter}>Twitter</a>, and spending time with my family.
     </p>
-    <b>
+    <strong>
       Currently available for hire.{" "}
       <GatsbyLink to={routes.contact}>
         Contact me if you're interested!
       </GatsbyLink>
-    </b>
+    </strong>
   </React.Fragment>
 )
 
