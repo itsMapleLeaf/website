5b5cdaeb6ea7f8bd93eafd5c01b41f6a526e7b25
kingdaro <kingdaro@gmail.com>
2018-03-08 13:50:34 -0500

    more fun dot stuff
    - movement based on device orientation or cursor
    - fading in and out instead of "popping"
    - add some playfield padding to account for the moment

--- src/dots.js
+++ src/dots.js
@@ -3,8 +3,18 @@ canvas.style.width = "100vw"
 canvas.style.height = "100vh"
 canvas.style.position = "absolute"
 
+function clamp(num, min, max) {
+  if (num < min) return min
+  if (num > max) return max
+  return num
+}
+
+function lerp(a, b, delta) {
+  return clamp(delta, 0, 1) * (b - a) + a
+}
+
 function randomRange(min, max) {
-  return Math.random() * (max - min) + min
+  return lerp(min, max, Math.random())
 }
 
 function resetCanvasSize() {
@@ -13,39 +23,81 @@ function resetCanvasSize() {
   canvas.height = rect.height
 }
 
+const mouse = { x: 0, y: 0 }
+
+window.addEventListener("mousemove", event => {
+  mouse.x = event.clientX
+  mouse.y = event.clientY
+})
+
+let orientation
+
+window.addEventListener("deviceorientation", event => {
+  orientation = {
+    x: event.gamma,
+    y: event.beta,
+  }
+})
+
 const dots = []
 const dotSize = 8
 const dotSpeed = 100
+const dotOffset = { x: 0, y: 0 }
+const playfieldPadding = 200
 
 function generateDots() {
   for (let i = 0; i < 100; i++) {
-    const x = Math.random() * canvas.width
-    const y = Math.random() * canvas.height
+    const x = randomRange(-playfieldPadding, canvas.width + playfieldPadding)
+    const y = randomRange(-playfieldPadding, canvas.height + playfieldPadding)
     const z = randomRange(0.2, 1)
-    dots.push({ x, y, z })
+    dots.push({ x, y, z, opacity: 0 })
   }
 }
 
-function moveDots(dt) {
+function updateDots(dt) {
   for (const dot of dots) {
     dot.y += dotSpeed * dot.z * dt
-    if (dot.y > canvas.height + 10) {
-      dot.y -= canvas.height + 20
+
+    if (dot.y > canvas.height + playfieldPadding) {
+      dot.opacity = Math.max(dot.opacity - dt / 0.5, 0)
+
+      if (dot.opacity <= 0) {
+        dot.y -= canvas.height + playfieldPadding * 2
+      }
+    } else {
+      dot.opacity = Math.min(dot.opacity + dt / 0.5, 1)
     }
   }
 }
 
+function calculateDotOffset(dt) {
+  if (orientation) {
+    dotOffset.x = lerp(dotOffset.x, orientation.x * 10, dt * 5)
+    dotOffset.y = lerp(dotOffset.y, orientation.y * 10, dt * 5)
+  } else {
+    dotOffset.x = lerp(dotOffset.x, (mouse.x - canvas.width / 2) / canvas.width * 50, dt * 5)
+    dotOffset.y = lerp(dotOffset.y, (mouse.y - canvas.height / 2) / canvas.height * 50, dt * 5)
+  }
+}
+
 function drawDots(context) {
-  context.fillStyle = "hsla(224, 73%, 97%, 0.2)"
   for (const dot of dots) {
+    context.fillStyle = `hsla(224, 73%, 97%, ${dot.opacity * dot.z * 0.2})`
     context.beginPath()
-    context.arc(dot.x, dot.y, dotSize * dot.z, 0, Math.PI * 2)
+    context.arc(
+      dot.x + dotOffset.x * dot.z,
+      dot.y + dotOffset.y * dot.z,
+      dotSize * dot.z,
+      0,
+      Math.PI * 2,
+    )
     context.fill()
   }
 }
 
 function update(dt) {
-  moveDots(dt)
+  updateDots(dt)
+  calculateDotOffset(dt)
 }
 
 function draw() {
@@ -53,6 +105,11 @@ function draw() {
   context.clearRect(0, 0, context.canvas.width, context.canvas.height)
 
   drawDots(context)
+
+  // if (orientation) {
+  //   context.fillStyle = "white"
+  //   context.fillText(`${orientation.x}, ${orientation.y}`, 10, 10)
+  // }
 }
 
 let lastTime
