3c509997dd3c29f052fe2a3f2697ac28900d3260
kingdaro <kingdaro@gmail.com>
2018-08-09 19:54:11 -0500

    use let

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,6 +1,6 @@
 // https://stackoverflow.com/a/6274381/1332403
 export function shuffle(array: any[]) {
-  var j, x, i
+  let j, x, i
   for (i = array.length - 1; i > 0; i--) {
     j = Math.floor(Math.random() * (i + 1))
     x = array[i]
