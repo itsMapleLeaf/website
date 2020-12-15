a70c6d1423fb1e47ab07f97eb0f71937e28da9b6
kingdaro <19603573+kingdaro@users.noreply.github.com>
2020-12-15 03:51:09 -0600

    tailwind

--- src/dots/canvas.ts
+++ src/dots/canvas.ts
@@ -1,10 +1,6 @@
 import { queryDom } from "../helpers"
 
 export const canvas = queryDom("canvas") as HTMLCanvasElement
-canvas.style.width = "100vw"
-canvas.style.height = "100vh"
-canvas.style.position = "absolute"
-canvas.style.pointerEvents = "none"
 
 export function resetCanvasSize() {
   const rect = canvas.getBoundingClientRect()
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+const colors = require("tailwindcss/colors")
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        orange: colors.orange,
+      },
+      fontFamily: {
+        sans: `"Fira Sans", sans-serif`,
+        condensed: `"Fira Sans Condensed", sans-serif`,
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
