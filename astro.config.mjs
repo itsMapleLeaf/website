b96d33642c21b8670329169b8e76eb34e00f3295
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-06-26 17:16:37 -0500

    add preact integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
+import preact from "@astrojs/preact"
 import { defineConfig } from "astro/config"
 
 // https://astro.build/config
-export default defineConfig({})
+export default defineConfig({
+  integrations: [preact()],
+})
