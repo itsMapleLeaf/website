8b9c6485badbcd3b78847dd367662de36c69d176
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-06-26 17:23:12 -0500

    use external link component instead of hacky script

--- src/links.ts
+++
@@ -1,8 +0,0 @@
-const links = document.querySelectorAll("a[href]")
-
-for (const link of Array.from(links)) {
-  if (link instanceof HTMLAnchorElement) {
-    link.target = "_blank"
-    link.rel = "noopener noreferrer"
-  }
-}
