013958e0ccd13d31d10eea284560652092b6f633
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-06-26 16:57:04 -0500

    convert to astro

---
+++ tailwind.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  content: ["src/**/*.{astro,ts,tsx,js,jsx,md}"],
+  theme: {
+    fontFamily: {
+      sans: `"Fira Sans", sans-serif`,
+      condensed: `"Fira Sans Condensed", sans-serif`,
+    },
+  },
+}
