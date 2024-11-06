7efe66798895ceee0b29c9fd8f7c88f0cd9c3844
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-11-06 13:41:53 -0600

    add sharp and deno adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import preact from "@astrojs/preact"
 import tailwind from "@astrojs/tailwind"
+import deno from "@deno/astro-adapter"
 import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact(), tailwind()],
-  output: "static",
+  output: "hybrid",
+  adapter: deno(),
 })
